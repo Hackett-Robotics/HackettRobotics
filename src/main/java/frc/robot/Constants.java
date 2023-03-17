85091694353507cdd801171919b72993a6c9e84d
Hackett Robotics <44179134+fuzzycoco@users.noreply.github.com>
2023-03-17 19:05:01 -0400

    hi ashley

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,4 +9,12 @@ public class Constants {
     //Tells the program that there are two different controllers connected
     public static final int xboxController1 = 0;
     public static final int xboxController2 = 1;
+
+    //Left motor group
+    public static final int leftDriver1 = 0;
+    public final static int leftDriver2 = 1;
+
+    //Right motor group
+    public final static int rightDriver1 = 3;
+    public final static int rightDriver2 = 4;
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,20 +19,18 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.cameraserver.CameraServer;
 
-
 public class Robot extends TimedRobot {
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
-  
+
   Timer timer;
 
   DifferentialDrive robotDrive;
 
-  PWMVictorSPX crane1, crane2, frontLeft, frontRight, backLeft, backRight, motor1, motor2;
+  PWMVictorSPX crane1, crane2, frontLeft, frontRight, backLeft, backRight, motor1, motor2, intake, m_crane1, m_crane2;
 
-  
-
+  MotorControllerGroup crane;
 
   XboxController xbox1;
   XboxController xbox2;
@@ -42,77 +40,93 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
 
-    //Starts camera
+    // Starts camera
     CameraServer.startAutomaticCapture();
-    
-    //Define the different controllers as seperate and distinct inputs
+
+    // Define the different controllers as seperate and distinct inputs
     xbox1 = new XboxController(Constants.xboxController1);
     xbox2 = new XboxController(Constants.xboxController2);
-    
-  //Tells the robot that we are using a tank system
-   MotorController m_frontLeft = new PWMVictorSPX(1);
-   MotorController m_rearLeft = new PWMVictorSPX(2);
-   MotorControllerGroup m_left = new MotorControllerGroup(m_frontLeft, m_rearLeft);
 
-   MotorController m_frontRight = new PWMVictorSPX(3);
-   MotorController m_rearRight = new PWMVictorSPX(4);
-   MotorControllerGroup m_right = new MotorControllerGroup(m_frontRight, m_rearRight);
+    // Tells the robot that we are using a tank system
+    MotorController m_frontLeft = new PWMVictorSPX(Constants.leftDriver1);
+    MotorController m_rearLeft = new PWMVictorSPX(Constants.leftDriver2);
+    MotorControllerGroup m_left = new MotorControllerGroup(m_frontLeft, m_rearLeft);
 
+    MotorController m_frontRight = new PWMVictorSPX(Constants.rightDriver1);
+    MotorController m_rearRight = new PWMVictorSPX(Constants.rightDriver2);
+    MotorControllerGroup m_right = new MotorControllerGroup(m_frontRight, m_rearRight);
 
-   /* 
-  MotorController m_crane1 = new PWMVictorSPX(5);
-  MotorController m_crane2 = new PWMVictorSPX(6);
-  MotorControllerGroup crane = new MotorControllerGroup(m_crane1, m_crane2);
-  */
+    MotorController m_crane1 = new PWMVictorSPX(5);
+    MotorController m_crane2 = new PWMVictorSPX(6);
+    crane = new MotorControllerGroup(m_crane1, m_crane2);
 
-  robotDrive = new DifferentialDrive(m_left, m_right);
+    intake = new PWMVictorSPX(7);
+
+    robotDrive = new DifferentialDrive(m_left, m_right);
   }
 
-
   boolean clawCon = true;
+  int intakeGo = 0;
+
   @Override
   public void teleopPeriodic() {
 
     double speedCap = 1;
-    robotDrive.tankDrive(-speedCap*xbox1.getRawAxis(1), speedCap*xbox1.getRawAxis(5));
+    robotDrive.tankDrive(-speedCap * xbox1.getRawAxis(1), speedCap * xbox1.getRawAxis(5));
 
-    crane1.set(xbox1.getLeftTriggerAxis());
-    crane2.set(xbox1.getLeftTriggerAxis());
+    double craneCap = 0.5;
+    if(xbox1.getLeftTriggerAxis() != 0.0){
+      crane.set(craneCap * xbox1.getLeftTriggerAxis());
+    }
+    else if(xbox1.getRightTriggerAxis() != 0.0){
+      crane.set(craneCap * -xbox1.getRightTriggerAxis());
+    }
+    
 
+    
+    if (xbox1.getAButtonReleased()) {
+      if (intakeGo == 0) {
+        intakeGo = 1;
+      } else {
+        intakeGo = 0;
+      }
+    }
+    intake.set(intakeGo);
+    
 
   }
 
   @Override
   public void autonomousInit() {
-    //m_autonomousCommand = m_chooser.getSelected();
+    // m_autonomousCommand = m_chooser.getSelected();
 
     robotDrive.setSafetyEnabled(false);
 
-    //Starts timer object
+    // Starts timer object
     timer = new Timer();
     timer.reset();
     timer.start();
-    
-    //Declaring variabled and arrays
-    double[] xSpeed =       {0    , 0    , 0.5 , 0   , 0    , -0.5};
-    double[] ySpeed =       {0    , 0    , 0   , 0   , 0    , 0};
-    double[] zSpeed =       {0    , 0    , 0   , 0   , 0    , 0};
-    double[] timeIntevals = {2    , 1    , 1   , 1   , 0    , 3};
-    boolean[] lauraHuges =  {false,false ,false,true , true , true}; //Claw
-    double[] vernonDouglas= {0    , -1   ,-0.5 , -0.5, 0    , 0};  //Crane
-    double[] samPound =     {-1   , 0    , 0   , 0   , 0    , 0};    //wench
+
+    // Declaring variabled and arrays
+    double[] xSpeed = { 0, 0, 0.5, 0, 0, -0.5 };
+    double[] ySpeed = { 0, 0, 0, 0, 0, 0 };
+    double[] zSpeed = { 0, 0, 0, 0, 0, 0 };
+    double[] timeIntevals = { 2, 1, 1, 1, 0, 3 };
+    boolean[] lauraHuges = { false, false, false, true, true, true }; // Claw
+    double[] vernonDouglas = { 0, -1, -0.5, -0.5, 0, 0 }; // Crane
+    double[] samPound = { -1, 0, 0, 0, 0, 0 }; // wench
     double autonTime;
-    
+
   }
 
   @Override
   public void autonomousPeriodic() {
     /*
-    robotDrive.driveCartesian(0, -0.25, 0);
-    Timer.delay(1);
-    robotDrive.driveCartesian(0, 0, 0);
-    /*
-    /*
+     * robotDrive.driveCartesian(0, -0.25, 0);
+     * Timer.delay(1);
+     * robotDrive.driveCartesian(0, 0, 0);
+     * /*
+     * /*
      * String autoSelected = SmartDashboard.getString("Auto Selector",
      * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
      * = new MyAutoCommand(); break; case "Default Auto": default:
